979a28e3c8d1d98c33618a5ff268c6690f08ee58
Fana Mihail <fanamihail@Fanas-MacBook-Air.local>
2020-04-26 22:55:34 +0100

    html fail

--- js/site.js
+++ js/site.js
@@ -1,3 +1,29 @@
-$(function (){
-    console.log('Jquery is Loaded');
-})
+// Create array
+var moviesName = ["Marriage Story ", "Rosemary’s Baby" , "Zodiac ", "The Irishman ", "Dolemite is My Name ", "El Camino ", "Step Brothers"];
+
+console.log(moviesName);
+
+//Reversed array
+var reversed = moviesName.reverse();
+console.log(reversed);
+
+//Add the movies on the end of the array
+var moviesName = ["Marriage Story ", "Rosemary’s Baby" , "Zodiac ", "The Irishman ", "Dolemite is My Name ", "El Camino ", "Step Brothers"];
+var addMovies = moviesName.push("Burning",);
+console.log(moviesName);
+
+// Add the movies on the start of the array my methode
+var moviesName = ["Marriage Story ", "Rosemary’s Baby" , "Zodiac ", "The Irishman ", "Dolemite is My Name ", "El Camino ", "Step Brothers"];
+var reversed = moviesName.reverse();
+var addMovies = moviesName.push("Roma",)
+var addStart = reversed.reverse();
+console.log(addStart);
+
+// Inserts a movie as the second item in the list
+var moviesName = ["Marriage Story ", "Rosemary’s Baby" , "Zodiac ", "The Irishman ", "Dolemite is My Name ", "El Camino ", "Step Brothers"];
+moviesName[1] = "Frances Ha";
+console.log(moviesName);
+
+// Inserts the list into the html page as as list of paragraphs (<p> tag)
+var moviesName = ["Marriage Story ", "Rosemary’s Baby" , "Zodiac ", "The Irishman ", "Dolemite is My Name ", "El Camino ", "Step Brothers"];
+
