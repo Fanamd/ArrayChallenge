8fcc0a53b549d1bce1fc4f99b440bc4b1db6c262
Fana Mihail <fanamihail@Fanas-MacBook-Air.local>
2020-04-28 20:44:57 +0100

    Challenge Array

--- js/site.js
+++ js/site.js
@@ -24,6 +24,34 @@ var moviesName = ["Marriage Story ", "Rosemary’s Baby" , "Zodiac ", "The Irish
 moviesName[1] = "Frances Ha";
 console.log(moviesName);
 
-// Inserts the list into the html page as as list of paragraphs (<p> tag)
-var moviesName = ["Marriage Story ", "Rosemary’s Baby" , "Zodiac ", "The Irishman ", "Dolemite is My Name ", "El Camino ", "Step Brothers"];
+// // Inserts the list into the html page as as list of paragraphs (<p> tag)
+var moviesName = ["Marriage Story ", "Rosemary’s Baby", "Zodiac ", "The Irishman ", "Dolemite is My Name ", "El Camino ", "Step Brothers"];
+htmlString = []
+console.log(moviesName);
+$(function () {
 
+    for (i = 0; i < moviesName.length; i++) {
+        console.log(`${moviesName[i]}`);
+        htmlString.push(`<p> ${moviesName[i]}</p>`);
+    }
+    $('#listMovies').html(htmlString.join(''));
+})
+
+
+// Inserts the list into the html page as an ORDERED (OL) list
+var moviesName = ["Marriage Story ", "Rosemary’s Baby", "Zodiac ", "The Irishman ", "Dolemite is My Name ", "El Camino ", "Step Brothers"];
+htmlString = []
+console.log(moviesName);
+$(function () {
+    htmlString.push(`<ol>`);
+    for (i = 0; i < moviesName.length; i++) {
+        console.log(`${moviesName[i]}`);
+        
+        htmlString.push(`<li>`);
+        htmlString.push(`<p> ${moviesName[i]}</p>`);
+        htmlString.push(`</li>`);
+        
+    }
+    htmlString.push(`</ol>`)
+    $('#listMovies').html(htmlString.join(''));
+})
